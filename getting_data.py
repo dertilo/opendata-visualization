fba35637ed4a980ec218d4bf8239542d474d047e
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-09-26 12:53:31 +0200

    encodings

--- getting_data.py
+++ getting_data.py
@@ -5,13 +5,14 @@ import camelot
 import re
 
 def read_jsonl(file):
-    with open(file,'r') as f:
-        data = [json.loads(l) for l in f]
+    with open(file,'rb') as f:
+        data = [json.loads(l.decode('utf-8')) for l in f]
     return data
 
 def write_jsonl(file, data):
-    with open(file, 'w') as f:
-        f.writelines([json.dumps(d) + '\n' for d in data])
+    with open(file, 'wb') as f:
+        strings = (json.dumps(d, ensure_ascii=False) + '\n' for d in data)
+        f.writelines((s.encode('utf-8') for s in strings))
 
 def corret_table_parsing_erros(datum):
     county, city = datum['Departamento'],datum['Municipio']
@@ -36,14 +37,15 @@ def parse_table_in_pdf(pdf_file):
     tables = camelot.read_pdf(pdf_file, pages='all', split_text=True)
     data = [row for table in tables for row in table.data[1:]]
     header = data.pop(0)
-    data = [{k: v for k, v in zip(header, datum)} for datum in data]
+    data = [{k: v.replace('\n','') if isinstance(v,str) else v for k, v in zip(header, datum)} for datum in data]
     [corret_table_parsing_erros(c) for c in data]
     return data
 
 def get_data(
-        pdf_file,
+        pdf_file=None,
         file='data.jsonl'):
     if not os.path.isfile(file):
+        assert pdf_file is not None
         data = parse_table_in_pdf(pdf_file)
         write_jsonl(file, data)
     else:
@@ -52,6 +54,6 @@ def get_data(
 
 if __name__ == '__main__':
     pdf_url = 'https://www.queremosdatos.co/request/418/response/856/attach/6/Homicidios2017%202018FINAL.pdf'
-    data = get_data(get_pdf_file(pdf_url=pdf_url))
+    data = get_data(get_pdf_file(pdf_url=pdf_url),file='data_1.jsonl')
     df = pandas.DataFrame(data=data)
     print(df[['Municipio','Apellidos','Fecha']][:3])
